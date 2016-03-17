e18364add0239698badd18f4a328782ce034450f
pawan <pawan.dhananjay@iiitb.org>
2016-03-18 01:27:30 +0530

    scatterplot from device data

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
-var db = mongojs('contactlist', ['contactlist']);
 var bodyParser = require('body-parser');
 var sdk = require("matrix-js-sdk");
+var async = require("async");
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
 var matrixClient;
+var roomlist;
 app.post('/login',function(req, res) {
   var body = req.body;
   var myUserId = body.user_id;
   var myAccessToken = body.access_token;
+
   matrixClient = sdk.createClient({
     baseUrl: "http://localhost:8008",
     accessToken: myAccessToken,
@@ -19,27 +21,42 @@ app.post('/login',function(req, res) {
   });
   console.log("starting");
   matrixClient.startClient();
-  matrixClient.on("Room.timeline", function(event, room, toStartOfTimeline) {
-       if (toStartOfTimeline) {
-           return; // don't print paginated results
-       }
-       if (event.getType() !== "m.room.message") {
-           return; // only print messages
-       }
-       console.log(
-           // the room name will update with m.room.name events automatically
-           "(%s) %s :: %s", room.name, event.getSender(), event.getContent().body
-       );
-   });
+});
+
+app.get('/roomNames',function (req, res) {
+  console.log("Get request");
+
+  var rooms = matrixClient.getRooms();
+  var roomNames = [];
+  for(var i=0;i<rooms.length;i++){
+    roomNames.push(rooms[i].name);
+  }
+  res.json(roomNames);
+  
+});
+
+app.post('/roomData',function (req, res) {
+  console.log("Post request");
+
+  var rooms = matrixClient.getRooms();
+  var history = [];
+  for(var i=0;i<rooms.length;i++){
+    var events = rooms[i].getLiveTimeline()._events;
+    var data = []; 
+    for(var j=0;j<events.length;j++){
+      if(events[j].getContent().body!=undefined)
+        data.push({"content":events[j].getContent().body, "sender": events[j].getSender()});
+    }
+    // history.push({"room":rooms[i].name,"data":data});
+    history.push(data);
+  }
+  console.log(history);
+  res.json(history);
 });
 
 app.listen(3000);
 console.log("server on port 3000");
 
-// var myUserId = "@pawan:127.0.0.1";
-// var myAccessToken = "MDAxN2xvY2F0aW9uIDEyNy4wLjAuMQowMDEzaWRlbnRpZmllciBrZXkKMDAxMGNpZCBnZW4gPSAxCjAwMjNjaWQgdXNlcl9pZCA9IEBwYXdhbjoxMjcuMC4wLjEKMDAxNmNpZCB0eXBlID0gYWNjZXNzCjAwMWRjaWQgdGltZSA8IDE0NTc5NjIyNjU2OTcKMDAyZnNpZ25hdHVyZSB0uTdWzsG_WSR4m6i05wqaFJhic3ytT0T07yHcvR_Dvgo";
-
-// 
 // // var clc = require("cli-color");
 // var matrixClient = sdk.createClient({
 //     baseUrl: "http://localhost:8008",
@@ -47,8 +64,18 @@ console.log("server on port 3000");
 //     userId: myUserId
 // });
 
-// console.log(matrixClient.getAccessToken());
 
-// 
+// matrixClient.on("Room.timeline", function(event, room, toStartOfTimeline) {
+//        if (toStartOfTimeline) {
+//            return; // don't print paginated results
+//        }
+//        if (event.getType() !== "m.room.message") {
+//            return; // only print messages
+//        }
+//        console.log(
+//            // the room name will update with m.room.name events automatically
+//            "(%s) %s :: %s", room.name, event.getSender(), event.getContent().body
+//        );
+//    });
 // console.log("starting");
 //    matrixClient.startClient();
--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -13,6 +13,11 @@ myApp.config(['$routeProvider',
       });
   }]);
 
+myApp.run(['$route', function($route)  {
+  $route.reload();
+}]);
+
+
 myApp.controller('LoginCtrl', ['$scope', '$http','$location', function($scope, $http, $location) {
 	console.log("Hello World from controller");
 
@@ -32,4 +37,83 @@ myApp.controller('LoginCtrl', ['$scope', '$http','$location', function($scope, $
 
 myApp.controller('HomeCtrl', ['$scope', '$http','$location', function($scope, $http, $location) {
 	console.log("Hello World from home controller");
+	
+	var refresh = function() {
+		$http.get('/roomNames').success(function(response) {
+    		console.log("I got the data I requested");
+    		$scope.roomNames = response;
+    	});
+	}
+	refresh();
+	var scatterPlot = function(data) {
+		var margin = {top: 20, right: 15, bottom: 60, left: 80}
+      , width = 960 - margin.left - margin.right
+      , height = 500 - margin.top - margin.bottom;
+    
+    var x = d3.scale.linear()
+              .domain([0, d3.max(data, function(d) { return d[0]; })])
+              .range([ 0, width ]);
+    
+    var y = d3.scale.linear()
+    	      .domain([0, d3.max(data, function(d) { return d[1]; })])
+    	      .range([ height, 0 ]);
+ 
+    var chart = d3.select('body')
+	.append('svg:svg')
+	.attr('width', width + margin.right + margin.left)
+	.attr('height', height + margin.top + margin.bottom)
+	.attr('class', 'chart')
+
+    var main = chart.append('g')
+	.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+	.attr('width', width)
+	.attr('height', height)
+	.attr('class', 'main')   
+        
+    // draw the x axis
+    var xAxis = d3.svg.axis()
+	.scale(x)
+	.orient('bottom');
+
+    main.append('g')
+	.attr('transform', 'translate(0,' + height + ')')
+	.attr('class', 'main axis date')
+	.call(xAxis);
+
+    // draw the y axis
+    var yAxis = d3.svg.axis()
+	.scale(y)
+	.orient('left');
+
+    main.append('g')
+	.attr('transform', 'translate(0,0)')
+	.attr('class', 'main axis date')
+	.call(yAxis);
+
+    var g = main.append("svg:g"); 
+    
+    g.selectAll("scatter-dots")
+      .data(data)
+      .enter().append("svg:circle")
+          .attr("cx", function (d,i) { return x(d[0]); } )
+          .attr("cy", function (d) { return y(d[1]); } )
+          .attr("r", 8);
+	}
+
+  	$scope.getRoomData = function(room){
+  		var data = JSON.stringify({'room':room});
+  		$http.post('roomData', data).success(function(response){
+  			var iotdata = response[0].map(function(key){
+  				return key.content;
+  			});
+  			var data = iotdata.map(function(r){
+  				return r.split(" ");
+  			});
+  			for(var i=0;i<data.length;i++){
+  				data[i] = data[i].map(Number);
+  			}
+  			scatterPlot(data);
+  			
+  		})		
+  	}
 }]);
